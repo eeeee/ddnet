841e7c1ea1e14b88bfa7240169176bea13162ff5
eeeee <eeeee@qwe123.info>
2014-11-30 02:27:31 -0800

    fix TimePassed for particles

--- src/game/client/components/particles.cpp
+++ src/game/client/components/particles.cpp
@@ -74,7 +74,9 @@ void CParticles::Add(int Group, CParticle *pPart)
 
 void CParticles::Update(float TimePassed)
 {
-	TimePassed = 1.0/100;
+	if (TimePassed < 1e-6) {
+		return;
+	}
 	static float FrictionFraction = 0;
 	FrictionFraction += TimePassed;
 
